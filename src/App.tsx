2668aa3eeb0f57dd890a9ae2929d37d71b2d97b4
Simon Bilodeau <simonbilodeau27@gmail.com>
2022-02-16 15:08:18 -0500

    xp amounts (dummy values)

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,18 @@ import Topmenubar from './MUIcomponents/topbarmenu'
 
 function App() {
   return (
-    <div className='App'>
-      <Topmenubar />
-      <Playermenu />
-      <Levelmenu />
+    <div>
+      <div className='App'>
+        <Topmenubar />
+      </div>
+      <div className='left'>
+        <Playermenu />
+        <Levelmenu />
+      </div>
+      <p>easy: 100 xp</p>
+      <p>medium: 200 xp</p>
+      <p>hard: 400 xp</p>
+      <p>deadly: 1000 xp</p>
     </div>
   )
 }
