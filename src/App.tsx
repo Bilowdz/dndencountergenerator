4f6a53c6ff80bb206576b304ed1a48b76b72a3ac
Simon Bilodeau <simonbilodeau27@gmail.com>
2022-02-23 13:52:23 -0500

    ADD size button context

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 import './App.css';
 import Playermenu from './MUIcomponents/dropmenu';
 import Levelmenu from './MUIcomponents/levelmenu';
-import Sizemenu from './MUIcomponents/size';
+import Sizemenu from './MUIcomponents/sizemenu';
 import Topmenubar from './MUIcomponents/topbarmenu';
 
 import { playerContext } from './contexts/playerContext';
 import { levelContext } from './contexts/levelContext';
+import { sizeContext } from './contexts/sizeContext';
 
 function App() {
   const [pContext, setPContext] = useState(1);
   const [lContext, setLContext] = useState(1);
+  const [sContext, setSContext] = useState('any');
 
   const [easyxp, setEasyxp] = useState(25);
   const [mediumxp, setMediumxp] = useState(50);
@@ -37,14 +39,23 @@ function App() {
             <Levelmenu />
           </levelContext.Provider>
           <p>{lContext}</p>
-          <Sizemenu />
+          <sizeContext.Provider value={[sContext, setSContext]}>
+            <Sizemenu />
+          </sizeContext.Provider>
+          <p>{sContext}</p>
         </div>
         <p>easy: {easyxp} xp</p>
         <p>medium: {mediumxp} xp</p>
         <p>hard: {hardxp} xp</p>
         <p>deadly: {deadlyxp} xp</p>
-        {/* <p>levelCount: {levelCount}</p> */}
-        {/* type, CR min/max, alignment, environment, legendary, sources. Just make buttons for now */}
+        <p>size: {sContext}</p>
+        {/* <p>type: </p>
+        <p>CR min: </p>
+        <p>CR max: </p>
+        <p>alignment: </p>
+        <p>environment: </p>
+        <p>legendary: </p>
+        <p>sources: </p> */}
       </div>
     </>
   );
