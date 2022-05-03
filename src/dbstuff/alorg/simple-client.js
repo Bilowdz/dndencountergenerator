8d744933745b6f0073fe505c41343773c9b3cfb3
Simon Bilodeau <simonbilodeau27@gmail.com>
2022-05-03 17:02:15 -0400

    removed the monster api stuff

--- src/dbstuff/alorg/simple-client.js
+++
@@ -1,4 +0,0 @@
-const { createClient } = require('@theoperatore/alorg-service');
-const client = createClient();
-
-client.get('alorg://dnd-monster-api/random').then(console.log);
--- src/dbstuff/db/utils.ts
+++
@@ -1,5 +0,0 @@
-import chalk from 'chalk';
-
-export const info = (...args: any[]) => console.log(chalk.blue('[info]'), ...args);
-export const error = (...args: any[]) => console.error(chalk.red('[error]'), ...args);
-export const success = (...args: any[]) => console.log(chalk.green('[success]'), ...args);
